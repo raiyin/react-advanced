2f76605508ade44a142595249848f09ce56d0134
Maxim P <raiyin@ya.ru>
2023-06-30 01:54:16 +0300

    Query and input

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,8 +5,17 @@ const HomePage = () => {
     const { isLoading, isError, data } = useSearchUsersQuery('vladilen');
 
     return (
-        <div>
-            home
+        <div className='flex justify-center pt-10 ms-auto h-screen w-screen'>
+            {isError && <p className='text-center text-red-600'>Something went wrong</p>}
+
+            <div className='relative w-[560px]'>
+                <input
+                    type="text"
+                    className='border py-2 px-4 w-full h-[42px] mb-2'
+                    placeholder='Search for Github username'
+                />
+                <div className='absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white'>Lorem ipsum</div>
+            </div>
         </div>
     );
 };
